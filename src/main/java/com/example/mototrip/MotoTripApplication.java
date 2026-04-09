c98c7eaa0d12d1d201c6fdda936542bc8db7e8ef
Mathieu ROBERT <mathieu.roobert91@gmail.com>
2026-04-09 13:41:18 +0200

    init

---
+++ src/main/java/com/example/mototrip/MotoTripApplication.java
@@ -0,0 +1,13 @@
+package com.example.mototrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mototrip/MotoTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mototrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
